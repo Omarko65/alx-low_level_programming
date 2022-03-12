fca047994d787a8d96c3fbd81f23e3e4612855f3
okor mark <okormark@gmail.com>
2022-03-12 21:59:42 +0100

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
 int main(void)
+
 {
-    
-for (int i = 0; i <= 9; i++)
+int digit;
+
+for (digit = 0 ; digit < 10; digit++)
 {
-printf("%d", i);
+printf("%digit", digit);
 }
+
 printf("\n");
-return 0;
+
+return (0);
+
 }
