eb03080d602c634d40d249d48c521b119b193d6e
okor mark <okormark@gmail.com>
2022-03-22 02:49:20 +0100

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - find string length
+ * @s: the array value
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+
+	}
+	return (length);
+}
