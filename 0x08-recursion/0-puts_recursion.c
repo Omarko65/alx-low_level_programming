01bb517417b3827967dbc57035045c83076111bd
okor mark <okormark@gmail.com>
2022-03-30 20:54:14 +0100

    HEader file and task 0 corrected

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to be printed
  */
 void _puts_recursion(char *s)
 {
-  _puts_recursion("Puts with recursion");
-  return (0);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
 }
