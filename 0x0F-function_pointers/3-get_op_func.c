64802356fb68249b3f8f2015bdb0510292e3b786
okor mark <okormark@gmail.com>
2022-04-15 00:32:45 +0100

    Task 3 correction

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,57 +1,28 @@
 #include "3-calc.h"
 
 /**
- * op_add - addition function
- * @a: first operand
- * @b: second operand
- * Return: the sum of a and b
- */
-int op_add(int a, int b)
+ * get_op_func -  selects the correct function to
+ * perform the operation asked by the user
+ * @s: input string
+ * Return: pointer to a function
+*/
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-/**
- * op_sub - subtraction function
- * @a: first operand
- * @b: second operand
- * Return: the difference btw a and b
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
+	while (i < 5)
+	{
+		if (!strcmp(ops[i].op, s))
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
-
-/**
- * op_mul - multiplication function
- * @a: first operand
- * @b: second operand
- * Return: the product of a and b
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - division function
- * @a: first operand
- * @b: second operand
- * Return: the quotient of a÷b
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-
-/**
- * op_mod - modulus function
- * @a: first operand
- * @b: second operand
- * Return: the remainder of a÷b
- */
-int op_mod(int a, int b)
-{
-	return (a % b);
-}
-
