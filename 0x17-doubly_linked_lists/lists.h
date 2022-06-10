34fdf140a04e8a7e50b30606130ff4c2ec4901b3
okor mark <okormark@gmail.com>
2022-06-11 00:35:03 +0100

    HEADER FIXED

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,57 +1,32 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * insert_node - insert node at given index
- * @tmp: ptr to nth position node in doubly linked list
- * @n: node data
- * Return: address of inserted node
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
  */
-dlistint_t *insert_node(dlistint_t *tmp, int n)
+typedef struct dlistint_s
 {
-	dlistint_t *new;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-	new = malloc(sizeof(struct dlistint_s));
-	if (!new)
-		return (NULL);
-	new->n = n;
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-	new->next = tmp;
-	new->prev = tmp->prev;
-	tmp->prev->next = new;
-	tmp->prev = new;
-
-	return (new);
-}
-
-/**
- * insert_dnodeint_at_index - create and insert node at nth index
- * @h: pointer to head of list
- * @idx: index
- * @n: node data
- * Return: address of inserted node, or NULL if failed
- */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{
-	dlistint_t *tmp;
-
-	/* insert at beginning if empty or existing linked list */
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (!h)
-		return (NULL);
-
-	/* insert in the middle of list */
-	tmp = *h;
-	while ((idx != 0) && (tmp->next))
-	{
-		idx -= 1;
-		tmp = tmp->next;
-		if (idx == 0)
-			return (insert_node(tmp, n));
-	}
-
-	/* insert at the end of list if idx is one after last node */
-	if (idx == 1)
-		return (add_dnodeint_end(h, n));
-	return (NULL);
-}
+#endif /* LISTS_H */
