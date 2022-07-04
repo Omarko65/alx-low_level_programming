41877df7f5b6a06d9b8f5f5eb3b9b13d65d07da7
holic65 <okormark@gmail.com>
2022-07-04 16:18:43 -0700

    makor

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,10 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	int i;
-	hash_table_t *hashTable[size];
-	
-	for(i = 0; i < size; i++)
-		hashTable[i] = NULL;
-	return (hashTable);
+	hash_table_t *hashTable;
+	unsigned long int i;
+
+	hashTable = malloc(sizeof(hash_table_t));
+
+	if (hashTable == NULL)
+		return NULL;
+
+	hashTable->size = size;
+	hashTable->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashTable->array == NULL)
+		return NULL;
+
+	for (i = 0; i < size; i++)
+		hashTable->array[i] = NULL;
+	return hashTable;
 }
