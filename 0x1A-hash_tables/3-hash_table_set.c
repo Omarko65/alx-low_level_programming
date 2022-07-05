f55bc00c55238c566140c8377a26ad4f001a014e
holic65 <okormark@gmail.com>
2022-07-05 01:30:39 -0700

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to a hash table
+ * @ht: the hash table that the element is added to
+ * @key: the key that will be used to raise the hashdjb2 code
+ * @value: the value that is to be passed to the table
+ *
+ * Return: return 1 if succeed or 0 if not
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *n, *n1;
+	char *_value;
+	unsigned long int index;
+	if(ht == NULL || key == NULL || value == NULL)
+		return 0;
+
+	_value = strdup(value);
+	if (_value == NULL)
+		return 0;
+	n = malloc(sizeof(hash_node_t));
+	if (n == NULL)
+		return 0;
+	n->next = NULL;
+	n->value = _value;
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+		ht->array[index] = n;
+	else
+	{
+		n1 = ht->array[index];
+		while(n1->next != NULL)
+			n1 = n1->next;
+		n1->next = n;
+	}
+	return 1;
+}
+		
