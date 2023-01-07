4b6b6cf1c3917524001438373cd88b9e3178454f
holic65 <okormark@gmail.com>
2023-01-07 14:17:14 -0800

    task header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,8 +6,11 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_arr(int *array, int left, int right);
+/*void print_arr(int *array, int left, int right);*/
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-
+int exponential_search(int *array, size_t sie, int value);
+int binary_look(int *array, size_t low, size_t high, int value);
+/*void print_arr(int *array, int left, int right);
+*/
 #endif
